f08ca0bdd4015813c0375097f4e81ec13e82557a
Pete Hamrick <pete.hamrick@gmail.com>
2021-12-06 11:10:19 -0800

    added home component, next to use NavLink in app.js

---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <article>
+      <h2>Welcome to the Library of Computer Science</h2>
+      <p>Check out our amazing selection in our catalog</p>
+    </article>
+  )
+}
